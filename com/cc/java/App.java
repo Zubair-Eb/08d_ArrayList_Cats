52c31e4f4c9122f16673da859ec87f208b1da6a0
Zubair <zubair.yousaf-eberl@gmx.de>
2022-04-27 13:04:41 +0200

    Objects with ArrayList

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,9 +1,35 @@
 package com.cc.java;
 
+import java.util.ArrayList;
+import java.util.jar.Attributes.Name;
+
 public class App {
     
     public static void main(String[] args) {
-     
+
+        // Vorüberlegung
+        // Cat cat1 = new Cat("Dessy");
+        // Cat cat2 = new Cat("Tom");
+        // Cat cat3 = new Cat("Flurry");
+        // Cat cat4 = new Cat("Dessy");
+
+        String [] nameArr = {"Tom","Timmy","Dizzy","Flurry","Bully"};
+
+        ArrayList<Cat> catList = new ArrayList<>();
+        
+       
+
+
+        for (int i = 0; i < nameArr.length; i++) {
+            // new Cat(nameArr[i]); // Objekte, Aber keine Referenzen
+
+            catList.add(new Cat(nameArr[i]));
+       
+            
+        }
+
+
+
     }
 
     public static void output(String outputStr) {
